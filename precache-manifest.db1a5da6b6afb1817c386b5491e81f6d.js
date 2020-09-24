6064f8a7509254fe263a377d53a807eebf8b5c1d
cdubyoo <chungwongg@gmail.com>
2020-09-24 13:59:16 -0700

    Updates

---
+++ precache-manifest.db1a5da6b6afb1817c386b5491e81f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8f340ba706346d76a6d4b70f12f901",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "368eb3c20980b60fa6d8",
+    "url": "/react-todo/static/css/main.92bd934a.chunk.css"
+  },
+  {
+    "revision": "67361699d8ca39151a38",
+    "url": "/react-todo/static/js/2.25e11dd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.25e11dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368eb3c20980b60fa6d8",
+    "url": "/react-todo/static/js/main.4c6aa28c.chunk.js"
+  },
+  {
+    "revision": "b1a3406d383bcab90b19",
+    "url": "/react-todo/static/js/runtime-main.1a7753af.js"
+  }
+]);
